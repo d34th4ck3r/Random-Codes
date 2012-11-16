49713c3b439a709dbe86008a79c13b5852188e27
Gautam Bajaj <gautam1237@gmail.com>
2012-11-16 19:44:37 +0530

    Palsquare USACO code corrected

--- USACO/palsquare.cpp
+++ USACO/palsquare.cpp
@@ -1,8 +1,14 @@
+/*
+ID: gautam11
+PROG: palsquare
+LANG: C++
+*/
 #include<iostream>
 #include<stdio.h>
 #include<stdlib.h>
 #include<vector>
 #include<string>
+#include<sstream>
 #include<map>
 #include<fstream>
 #include<cstdio>
@@ -30,21 +36,53 @@ typedef struct BinaryTree{
 
 using namespace std;
 
-bool isPalindrome(int N)
+bool isPalindrome(string N)
 {
-	int i=1;
-	int p=pow(10,i);
-	while(
+	int left=0;
+	int right=N.size()-1;
+	while(left<=right && N[left]==N[right])
+	{
+		left++;
+		right--;
+	}
+	if(left>=right)
+		return 1;
+	else
+		return 0;
 }
-
+string intTostring(int N)
+{
+	ostringstream out;
+	out<<N;
+	return out.str();
+}
+string convertBase(int N,int B,char Val[])
+{
+	int temp=N;
+	string sN="";
+	while(temp)
+	{
+		int remainder=temp%B;
+		sN=Val[remainder]+sN;
+		temp=temp/B;
+	}
+	return sN;
+}
 int main()
 {
-	int N;
-	Sint(N);
-	int Sq[301];
-	for(int i=1;i<301;i++)
+	ifstream fin("palsquare.in");
+	ofstream fout("palsquare.out");
+	char Val[20]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J'};
+	int B;
+	fin>>B;
+	for(int i=1;i<=300;i++)
 	{
-		Sq[i]=i*i;
+		string CBS=convertBase(i*i,B,Val);
+		string CB=convertBase(i,B,Val);
+		if(isPalindrome(CBS))
+		{
+			fout<<CB<<" "<<CBS<<endl;
+		}
 	}
 	return 0;
 }
